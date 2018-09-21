8291709919e80eda020bb7023f9f334d3a11820b
Zach Funderburk <zf547414@ohio.edu>
2018-09-21 12:16:14 -0400

    Dialogue Start

--- Come On In/Assets/_Scripts/Dialogue(New)/NPC.cs
+++ Come On In/Assets/_Scripts/Dialogue(New)/NPC.cs
@@ -1,16 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NPC : MonoBehaviour {
+public class NPC : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
-		
+    public Transform TextBG;
+    public Transform NonPlayer;
+
+    private DialogueSystem dialogueSystem;
+
+    public string Name;
+
+    [TextArea(5, 10)]
+    public string[] sentences;
+
+	void Start () 
+    {
+        dialogueSystem = FindObjectOfType<DialogueSystem>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update () 
+    {
+        //		TextBG.position = Camera.main.worldToScreenPoint(NonPlayer.position + Vector3.up * 7f)
+        Vector3 Pos = Camera.main.WorldToScreenPoint(NonPlayer.position);
+        Pos.y += 175;
+        TextBG.position = Pos;
 	}
+
+    public void OnTriggerStay(Collider other)
+    {
+        this.gameObject.GetComponent<NPC>().enabled = true;
+        FindObjectOfType<DialogueSystem>().EnterRangeOfNPC();
+        if ((other.gameObject.tag == "Player") && Input.GetKeyDown(KeyCode.Space))
+        {
+            this.gameObject.GetComponent<NPC>().enabled = true;
+            dialogueSystem.Names = Name;
+            dialogueSystem.dialogueLines = sentences;
+            FindObjectOfType<DialogueSystem>().NPCName();
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        FindObjectOfType<DialogueSystem>().OutOfRange();
+        this.gameObject.GetComponent<NPC>().enabled = false;
+    }
 }
