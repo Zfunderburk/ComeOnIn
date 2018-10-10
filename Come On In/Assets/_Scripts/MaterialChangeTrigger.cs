95e5993ecafcf0b918cd3f9480630d74ab48e76e
Zach Funderburk <zfunderburk21@gmail.com>
2018-10-10 18:31:10 -0400

    Transparent Wall beginnings

---
+++ Come On In/Assets/_Scripts/MaterialChangeTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChangeTrigger : MonoBehaviour {
+
+    public Material baseMaterial;
+    public Material fadeMaterial;
+
+    void OnTriggerEnter (Collider other)
+    {
+        Debug.Log("Player has entered the trigger");
+
+        if(other.gameObject.tag == "Player")
+        {
+            GetComponent<Renderer>().material = fadeMaterial;
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        Debug.Log("Player has exited the trigger");
+
+        if(other.gameObject.tag == "Player")
+        {
+            GetComponent<Renderer>().material = baseMaterial;
+        }
+    }
+
+}
